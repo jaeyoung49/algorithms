5a3462e6c74bc234ac19a443f0a29054ad83759f
jaeyoung49 <jaeyoung49@gmail.com>
2016-11-13 23:02:23 +0900

    1 day 1 algorithms
    Heapsort - modify.
    Heapsort : PriorityQueue - update.

--- ch06-heapsort/src/step1_heapsort/Heapsort.java
+++ ch06-heapsort/src/step1_heapsort/Heapsort.java
@@ -10,46 +10,33 @@ public class Heapsort {
 		System.out.println();
 	}
 	
-	public static void heapsort(int[] A){
-		int heapSize = A.length-1;
-		while(heapSize>0){
-			// 3. MaxHeapify(A, 1);
-			for(int j=heapSize; j>0; j--){
-				maxHeapify(A, j, heapSize);
-				printArray(array);
-			}
-			
-			// 1. exchange A[1] <-> A[i]
-			int temp = A[1];
-			A[1] = A[heapSize];
-			A[heapSize] = temp;
-			
-			// 2. heapSize <- heapSize-1
-			heapSize--;
-			
+	public static void buildMaxHeap(int[] A){
+		int heapSize = A.length;
+		for(int i = heapSize/2; i>0; i--){
+			maxHeapify(A, i, heapSize);
 		}
 	}
-		
 	
-	public static void maxHeapify(int[] A, int pivot, int heapSize){
+	public static void maxHeapify(int[] A, int i, int heapSize){
+		int pivot = i;
 		int left = (pivot)*2;
 		int right = (pivot)*2 + 1;
 		int maxChild = left;
 		//while A[i] has a child do
 		while(left <= heapSize){
 			// k <- index of the biggest child of i;
-			if( (pivot*2+1) <= heapSize)
-				if(A[left]<A[right])
+			if( right <= heapSize)
+				if(A[left-1] < A[right-1])
 					maxChild = right;
 			
 			// if A[i] >= A[k] ---> return;
-			if(A[maxChild]<=A[pivot])
+			if(A[maxChild-1]<=A[pivot-1])
 				return;
 			
 			// exchange A[i] and A[k]; 
-			int max = A[maxChild];
-			A[maxChild] = A[pivot];
-			A[pivot] = max;
+			int max = A[maxChild-1];
+			A[maxChild-1] = A[pivot-1];
+			A[pivot-1] = max;
 			
 			// i = k;
 			pivot = maxChild;
@@ -58,16 +45,40 @@ public class Heapsort {
 			maxChild = left;
 		}
 	}
+
+	public static void heapsort(int[] A){
+		// 1. Build-Max-Heap(A)
+		buildMaxHeap(A);
+		int heapSize = A.length;
+		
+		// 2. for i <- heap_size down to 2 do
+		for(int i=heapSize; i>1; i--){
+			// 3. exchange A[1] <-> A[i]	
+			int temp = A[0];
+			A[0] = A[i-1];
+			A[i-1] = temp;
+			
+			// 4. heap_size <- heap_size-1
+			heapSize--;
+						
+			// 5. Max-Heapify(A, 1)
+			maxHeapify(A, 1, heapSize);
+		}
+	}
 	
 	public static void main(String[] args) {
 		printArray(array);
+
 //		buildMaxHeap(array);
-		heapsort(array);
+		
 //		maxHeapify(array, 5);
 //		maxHeapify(array, 4);
 //		maxHeapify(array, 3);
 //		maxHeapify(array, 2);
 //		maxHeapify(array, 1);
+		
+		heapsort(array);
+		
 		printArray(array);
 	}
 
--- ch06-heapsort/src/step2_priority_queue/MaximumPriorityQueue.java
+++ ch06-heapsort/src/step2_priority_queue/MaximumPriorityQueue.java
@@ -13,39 +13,26 @@ public class MaximumPriorityQueue {
 		System.out.println();
 	}
 	
-	public static void maxHeapInsert(ArrayList<Integer> A, int key){
-		int heapSize = (A.size()-1) + 1;				// heap_size = heap_size + 1;
-		A.add(key);							// A[heap_size] = key;
-		int i = heapSize;							// i = heap_size;
-		int parent = i/2;								
-		while(i>1 && A.get(parent) < A.get(i)){				// while (i>1 and A[Parent(i)] < A[i] )
-			int temp = A.get(i);						// 		exchange A[i] and A[Parent(i)];
-			A.set(i, A.get(parent));
-			A.set(parent, temp);
-			i = parent;								// i = Parent(i);
-			parent = i/2;
-		}
-	}
-	
-	public static void maxHeapify(ArrayList<Integer> A, int pivot, int heapSize){
+	public static void maxHeapify(ArrayList<Integer> A, int pivot){
+		int heapSize = A.size()-1;
 		int left = (pivot)*2;
 		int right = (pivot)*2 + 1;
 		int maxChild = left;
 		//while A[i] has a child do
 		while(left <= heapSize){
 			// k <- index of the biggest child of i;
-			if( (pivot*2+1) <= heapSize)
-				if(A.get(left)<A.get(right))
+			if( right <= heapSize)
+				if(A.get(left-1)<A.get(right-1))
 					maxChild = right;
 			
 			// if A[i] >= A[k] ---> return;
-			if(A.get(maxChild)<=A.get(pivot))
+			if(A.get(maxChild-1)<=A.get(pivot-1))
 				return;
 			
 			// exchange A[i] and A[k]; 
-			int max = A.get(maxChild);
-			A.set(maxChild, A.get(pivot));
-			A.set(pivot, max);
+			int max = A.get(maxChild-1);
+			A.set(maxChild-1, A.get(pivot-1));
+			A.set(pivot-1, max);
 			
 			// i = k;
 			pivot = maxChild;
@@ -55,9 +42,37 @@ public class MaximumPriorityQueue {
 		}
 	}
 	
-	public static void main(String[] args) {
-		list.add(0);
+	public static void maxHeapInsert(ArrayList<Integer> A, int key){
+		int heapSize = A.size() + 1;				// heap_size = heap_size + 1;
+		A.add(key);									// A[heap_size] = key;
+		int ins = heapSize;							// i = heap_size;
+		int parent = ins/2;								
+		while(ins>1 && A.get(parent-1) < A.get(ins-1)){	// while (i>1 and A[Parent(i)] < A[i] )
+			int temp = A.get(ins-1);					//		exchange A[i] and A[Parent(i)];
+			A.set(ins-1, A.get(parent-1));
+			A.set(parent-1, temp);
+			ins = parent;								// i = Parent(i);
+			parent = ins/2;
+		}
+	}
+	
+	public static int heapExtractMax(ArrayList<Integer> A){
+		int heapSize = A.size();
+		int maxNumIndex = 1;
 		
+		if(heapSize<1){								// if heap-size[A] < 1
+			System.out.println("heap underflow");	//	then error "heap underflow"
+			return -1;
+		} else {
+			int maxNum = A.get(maxNumIndex-1);			// max <- A[1]
+			A.set(maxNumIndex-1, A.get(heapSize-1));	// A[1] <- A[heap-size[A]]
+			A.remove(heapSize-1);						// heap-size[A] <- heap-size[A] - 1
+			maxHeapify(A, 1); 							// MAX-HEAPIFY(A, 1)
+			return maxNum;
+		}
+	}
+	
+	public static void main(String[] args) {
 		list.add(20);
 		list.add(10);
 		list.add(15);
@@ -70,9 +85,10 @@ public class MaximumPriorityQueue {
 		list.add(5);
 		list.add(6);
 		
-		list.add(0);
 		System.out.println(list);
-//		maxHeapInsert(A, key);
+		maxHeapInsert(list, 30);
+		System.out.println(list);
+		System.out.println(heapExtractMax(list));
 		System.out.println(list);
 	}
 
